c09ee721a14a761fa899bba73c53059a3b311c63
Theo <theoparent71@gmail.com>
2021-12-01 13:59:10 +0100

    // WIP \\ obstacles.js

---
+++ js/obstacles.js
@@ -0,0 +1,19 @@
+
+
+function spawnObstacles(gameObject,speed) {
+
+    obstacles = [
+        '/assets/obstacles/cactus.png',
+        '/assets/obstacles/bigCactus.png',
+        '/assets/obstacles/rock',
+        '/assets/obstacles/bigRock.png',
+        '/assets/obstacles/plane.png',
+        '/assets/obstacles/spike.png'
+    ]
+
+
+    let random = Math.floor(Math.random() * obstacles.length + 1)
+
+
+
+}
